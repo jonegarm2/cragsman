6c53d82c68c7c7c264ef653e0c72de05a13d3c21
Jon Garman <jonegarm2@gmail.com>
2018-05-30 19:03:16 -0700

    readme

--- public/Photos.js
+++ public/Photos.js
@@ -4,14 +4,12 @@ class Photos extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cart: [], 
-            products: [],
-            // departments: [],
-            // similarItems: [],
-            // brands: [], 
-            userSearch: "", 
-            searchItems: []
-        };
-    }
-
-export default photos
+            photos: [
+                photo[0]: 'https://i.imgur.com/KPzaGMr.png',
+                photo[1]: 'https://i.imgur.com/GIfTRFS.png',
+                photo[2]: 'https://i.imgur.com/v2MCxBa.png',
+                photo[3]: 'https://i.imgur.com/Y8ofY6A.png',
+                photo[4]: 'https://i.imgur.com/w5JBne8.png'
+            ];
+        }
+    };
