1b6362bd4c6b087d064878003255b31dfd81eb9e
Jon Garman <jonegarm2@gmail.com>
2018-06-01 14:53:35 -0700

    remove favicon

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors')
@@ -12,7 +11,6 @@ var app = express();
 
 app.use(logger('dev'));
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(bodyParser.json());
